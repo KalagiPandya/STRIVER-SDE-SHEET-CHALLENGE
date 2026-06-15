66e6b94af5b1608faa0695bfdb68bfdb2e9850f1
KalagiPandya <pandyakalagi@gmail.com>
2026-06-15 19:55:43 +0530

    Day 14: Added Trapping Rainwater optimal solution

---
+++ Linkedlist/40_Trapping_Rainwater_Test.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+#include "40_Trapping_Rainwater.cpp"
+
+int main() {
+
+    vector<int> height =
+    {0,1,0,2,1,0,1,3,2,1,2,1};
+
+    Solution obj;
+
+    cout << obj.trap(height);
+
+    return 0;
+}
